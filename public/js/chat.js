dc66d2c97bc27dde4d6ce0a00ed82fe6d9e96cb1
TristanHeckelsmueller <tristan.heckelsmueller@cursade.de>
2019-03-06 17:38:19 +0100

    Install prompt

--- public/js/chat.js
+++ public/js/chat.js
@@ -112,3 +112,16 @@ locationButton.on('click', function () {
     });
 });
 
+window.addEventListener('beforeinstallprompt', e => {
+    console.log('beforeinstallprompt Event fired');
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    this.deferredPrompt = e;
+    return false;
+    });
+  // When you want to trigger prompt:
+  this.deferredPrompt.prompt();
+    this.deferredPrompt.userChoice.then(choice => {
+    console.log(choice);
+    });
+  this.deferredPrompt = null;
